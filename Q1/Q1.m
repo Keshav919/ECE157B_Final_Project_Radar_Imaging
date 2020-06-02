2b10680f4e089cd98015b952ff0458ec2db2671a
RenjieZhao <r2zhao@eng.ucsd.edu>
2020-06-02 14:39:45 -0700

    Update Q1.m

--- Q1/Q1.m
+++ Q1/Q1.m
@@ -20,24 +20,25 @@ B = k*N_sample*Ts;
 tau_resolution = 1/B;
 
 
-
 lambda = c/f_start;
 nRx = 4; % number of antennas
 sRx = lambda/2;% separation between receivers
 
+for i = 1:4
+    load("adcData_Q1data"+i+".mat");
+    adc_sampled = adcData_breathing_heart;
+    %find peak from the rangeFFT
+    RangeFFT = fft(adc_sampled,N_sample,2);%??????
+    ant = 1;
+    figure; 
+    plot(abs(mean(diff(RangeFFT(:,:,ant),1),1)))
 
-load('adcData_Q1data1.mat');
-adc_sampled = adcData_breathing_heart;
-%find peak from the rangeFFT
-RangeFFT = fft(adc_sampled,N_sample,2);%??????
-figure; 
-plot(abs(mean(diff(RangeFFT(:,:,2),1),1)))
+    [~, peak] = max(abs(mean(diff(RangeFFT(:,:,ant),1),1)));%??????
+    fprintf("The peak is at index"+peak+".\n")
 
-[~, peak] = max(abs(mean(diff(RangeFFT(:,:,2),1),1)));%??????
-
-
-bhFFT = fftshift(fft(unwrap(angle(RangeFFT(:,peak,2)))));
-f = 1/T_frame*(-length(bhFFT)/2:length(bhFFT)/2-1)/length(bhFFT);
-figure
-stem(f,10*log10(abs(bhFFT)))
-xlim([-2,2])
+    bhFFT = fftshift(fft(unwrap(angle(RangeFFT(:,peak,2)))));
+    f = 1/T_frame*(-length(bhFFT)/2:length(bhFFT)/2-1)/length(bhFFT);
+    figure
+    stem(f,10*log10(abs(bhFFT)))
+    xlim([-2,2])
+end
