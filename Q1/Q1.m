1ccee01134273ce88b7e3b83591509d2e42b17ae
RenjieZhao <r2zhao@eng.ucsd.edu>
2020-06-09 21:54:25 -0700

    Update Q1 and Q4

--- Q1/Q1.m
+++ Q1/Q1.m
@@ -27,28 +27,58 @@ sRx = lambda/2;% separation between receivers
 for i = 1:4
     load("adcData_Q1data"+i+".mat");
     adc_sampled = adcData_breathing_heart;
-    %find peak from the rangeFFT
-    RangeFFT = fft(adc_sampled,N_sample,2);%??????
+    
+    % rangeFFT
+    RangeFFT = fft(adc_sampled,N_sample,2);
     ant = 1;
+    f_max = 1/Ts; %Maximum frequency we can estimate
+    tau_max = f_max/k;
+    tau_range = 0:tau_resolution:tau_max-tau_resolution;
+    distance_range = c*tau_range/2;
+    %figure; 
+    %plot(abs(mean(diff(RangeFFT(:,:,ant),1),1)))
+    %[~, peak] = max(abs(mean(diff(RangeFFT(:,:,ant),1),1)));
+    %fprintf("Dataset "+i+". The peak is at index "+peak+".\n")
+    
+    % angle range
+    theta_range = asind(lambda/2/sRx);%From the lecture
+    theta_resolution = asind(lambda/nRx/sRx);%From the lecture
+
+    Nfft = 64; % Number of FFT points for AngleFFT
+    u = (-Nfft/2:Nfft/2-1)/Nfft*min([1,2*sin(theta_range)]);% x-axis in world of sin(theta)*(sep/lambda)
+    angle_vals = asind(u/sRx*lambda); % theta
+
+    AngleRangeFFT = fftshift(fft(RangeFFT,Nfft,3),3);
     figure; 
-    plot(abs(mean(diff(RangeFFT(:,:,ant),1),1)))
-
-    [~, peak] = max(abs(mean(diff(RangeFFT(:,:,ant),1),1)));%??????
-    fprintf("Dataset "+i+". The peak is at index "+peak+".\n")
-
-    bhFFT = fftshift(fft(unwrap(angle(RangeFFT(1:end-mod(size(RangeFFT,1),2),peak,2)))));
+    absdiff_anglerange = squeeze(abs(mean(diff(AngleRangeFFT,1,1),1)))';
+    surface(distance_range,angle_vals,absdiff_anglerange);
+    xlim([0 10])
+    xlabel("Distance")
+    ylabel("Angle")
+    
+    [angle_idx,d_idx] = find(absdiff_anglerange == max(absdiff_anglerange,[],'all'));
+    fprintf("In Dataset "+i+", the person is at the location with " + distance_range(d_idx) + "m distance and "+ angle_vals(angle_idx) + " degree angle.\n")
+    
+    bhFFT = fftshift(fft(unwrap(angle(AngleRangeFFT(1:end-mod(size(RangeFFT,1),2),d_idx,angle_idx)))));
     f = 1/T_frame*(-length(bhFFT)/2:length(bhFFT)/2-1)/length(bhFFT);
     figure
     stem(f,10*log10(abs(bhFFT)))
     xlim([-2,2])
     
-    %bfrange = find((f>0.2) & (f<0.42));
+    % breathing peak finding
     bfiltered = abs(bhFFT);
-    bfiltered((f<0.2) | (f>0.42)) = 0;
-    [~, b_idx] = max(bfiltered);    
+    bfiltered((f<0.1) | (f>0.5)) = 0;
+    [~, b_unfilt_idx] = findpeaks(bfiltered);
+    b_filtered_idx = b_unfilt_idx((b_unfilt_idx>max(find(f<0.21))) & (b_unfilt_idx<min(find(f>0.42))));
+    [~, b_filt_idx] = max(bfiltered(b_filtered_idx));
+    b_idx = b_filtered_idx(b_filt_idx);
+    % heart peak finding
     hfiltered = abs(bhFFT);
-    hfiltered((f<1) | (f>1.5)) = 0;
-    [~, h_idx] = max(hfiltered);   
+    hfiltered((f<0.9) | (f>1.6)) = 0;
+    [~, h_unfilt_idx] = findpeaks(hfiltered);
+    h_filtered_idx = h_unfilt_idx((h_unfilt_idx>max(find(f<1.1))) & (h_unfilt_idx<min(find(f>1.5))));
+    [~, h_filt_idx] = max(hfiltered(h_filtered_idx));
+    h_idx = h_filtered_idx(h_filt_idx);
 
-    fprintf("The breathing rate is "+f(b_idx)+"Hz. The heart rate is "+f(h_idx)+"Hz.\n")
+    fprintf("The breathing rate is "+f(b_idx)+"Hz. The heart rate is "+f(h_idx)+"Hz.\n\n")
 end
--- Q4/Q4.m
+++ Q4/Q4.m
@@ -1,42 +1,99 @@
 clear all; close all; clc
 
 %% load all the data
-adcData_breathing_heart_r1 = load('slow_walking_random.mat').adcData_breathing_heart;
-adcData_breathing_heart_r2 = load('slow_walking_random2.mat').adcData_breathing_heart;
-adcData_breathing_heart_t1 = load('slow_walking_towards.mat').adcData_breathing_heart;
-adcData_breathing_heart_t2 = load('slow_walking_towards2.mat').adcData_breathing_heart;
-adcData_breathing_heart_t3 = load('slow_walking_towards3.mat').adcData_breathing_heart;
-adcData_breathing_heart_t4 = load('slow_walking_towards4.mat').adcData_breathing_heart;
+load('slow_walking_random.mat')
+adcData_breathing_heart_r1 = adcData_breathing_heart;
+load('slow_walking_random2.mat')
+adcData_breathing_heart_r2 = adcData_breathing_heart;
+load('slow_walking_towards.mat')
+adcData_breathing_heart_t1 = adcData_breathing_heart;
+load('slow_walking_towards2.mat')
+adcData_breathing_heart_t2 = adcData_breathing_heart;
+load('slow_walking_towards3.mat')
+adcData_breathing_heart_t3 = adcData_breathing_heart;
+load('slow_walking_towards4.mat')
+adcData_breathing_heart_t4 = adcData_breathing_heart;
 
 %% view all the data to see what is going on
 
 plot(real(adcData_breathing_heart_r1(:,:,1)))
 
 %% constants
-
 f_start = 77e9;
-k = 79;
-t_chirp = 10e-3;
-fs = 1/t_chirp;
 
-N_samples = 256;
+k = 79e12;
+c = 3e8;
 
-fs_adc = 8000e3;
-ts_adc = 1/fs_adc;
+N_sample = 256;
+fs = 8e6;
+Ts = 1/fs;
+T = N_sample*Ts;
+
+T_frame = 10e-3;
 M = 3000;
 
-Nrx = 4;
+t_sample = (0:N_sample-1)*Ts;
 
-c=3e8;
+B = k*N_sample*Ts;
+tau_resolution = 1/B;
 
+lambda = c/f_start;
+nRx = 4; % number of antennas
+sRx = lambda/2;% separation between receivers
+
+ant = 1;
 
 %% Range FFT
 
-data_single_RX = adcData_breathing_heart_r1(:,:,1);
-f = zeros(size(data_single_RX,1), N_samples);
+adc_sampled = adcData_breathing_heart_r2;
+RangeFFT = fft(adc_sampled(:,:,ant),N_sample,2);
+N_frame = size(adc_sampled,1);
+t_frame = (1:size(adc_sampled,1))*T_frame;
 
-for ii=1:size(data_single_RX,1)
-    f(ii,:) = fft(data_single_RX(ii,:),N_samples);
-end
-% figure()
-imagesc(abs(f))
+f_max = 1/Ts; %Maximum frequency we can estimate
+tau_max = f_max/k;
+tau_range = 0:tau_resolution:tau_max-tau_resolution;
+distance_range = c*tau_range/2;
+
+figure
+imagesc(distance_range, t_frame, abs(RangeFFT))
+
+figure
+imagesc(distance_range, t_frame, abs(diff(RangeFFT)))
+
+[~, range_idx] = max(abs(diff(RangeFFT)),[],2);
+range_idx = [range_idx(1); range_idx];
+
+% %% Angle FFT
+% 
+% theta_range = asind(lambda/2/sRx);%From the lecture
+% theta_resolution = asind(lambda/nRx/sRx);%From the lecture
+% 
+% Nfft = 64; % Number of FFT points for AngleFFT
+% u = (-Nfft/2:Nfft/2-1)/Nfft*min([1,2*sin(theta_range)]);%% x-axis in world of sin(theta)*(sep/lambda)
+% angle_vals = asind(u/sRx*lambda);%%x--axis for theta
+% 
+% RangeFFT_rmrange = zeros(size(adc_sampled,1), size(adc_sampled,3));
+% for i = 1:size(adc_sampled,1)
+%     RangeFFT_rmrange = squeeze(RangeFFT(i,range_idx,:));
+% end
+% 
+% AngleRangeFFT = fftshift(fft(RangeFFT_rmrange,Nfft,2),2);
+% figure; 
+% surface(angle_vals,t_frame,abs(AngleRangeFFT),'EdgeColor','none');
+% %xlim([0 10])
+% xlabel("Time")
+% ylabel("Angle")
+% 
+% [~, angle_idx] = max(abs(AngleRangeFFT),[],2);
+
+%% Doppler FFT
+
+DopplerFFT = fft(RangeFFT,N_frame,1);
+max_index=find(RangeFFT(1,:)==max(RangeFFT(1,:)));
+v_max = (c/(2*f_start))*(f_breathing_samp);
+v_res = v_max/(N_chirps);
+y_axis = v_res:v_res:v_max;
+figure; imagesc(distance_range,y_axis,abs(DopplerFFT));
+xlabel('Range')
+ylabel('doppler')
